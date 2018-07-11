59ad2784b579f08eab7c68f0c9648649d3c55e04
wangwei <wangweei@gmail.com>
2018-07-11 23:23:55 +0800

    Change C code to C++

--- Source/Main.cc
+++ Source/Main.cc
@@ -8,7 +8,7 @@
 
 
 #include <iostream>
-#include <cstdio>
+// #include <cstdio>
 #include <cstdlib>
 #include <chrono>
 #include <thread>
@@ -19,45 +19,51 @@
 const unsigned MAX_ATTEMPTS = 50;
 const unsigned SLEEP_TIME = 10;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
 
-        const char* host = argc > 1 ? argv[1] : "";
+        const char *host = argc > 1 ? argv[1] : "";
         unsigned int port = argc > 2 ? atoi(argv[2]) : 0;
         if (port <= 0)
+        {
                 port = 7497;
-        const char* connectOptions = argc > 3 ? argv[3] : "";
+        }
+        const char *connectOptions = argc > 3 ? argv[3] : "";
         int clientId = 0;
         unsigned attempt = 0;
-        printf("Start of C++ Socket Client Test %u\n", attempt);
-        
-        while (true) {
+        // printf("Start of C++ Socket Client Test %u\n", attempt);
+
+        while (true)
+        {
                 ++attempt;
-                printf("Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
-                
+                // printf("Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
+
                 TestCppClient client;
-                
-                if (connectOptions) {
+
+                if (connectOptions)
+                {
                         client.setConnectOptions(connectOptions);
                 }
-                
+
                 client.connect(host, port, clientId);
-                printf("Shit, I had knew how to use this application...\n");
+                std::cout << "Shit, I had know how to use this application..." << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(5));
                 client.historicalDataRequests();
-                
-                while (client.isConnected()) {
+
+                while (client.isConnected())
+                {
                         client.processMessages();
                 }
-                if (attempt >= MAX_ATTEMPTS) {
+                if (attempt >= MAX_ATTEMPTS)
+                {
                         break;
                 }
-                
-                
-                printf("Sleeping %u seconds before next attempt\n", SLEEP_TIME);
+
+                // printf("Sleeping %u seconds before next attempt\n", SLEEP_TIME);
                 std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
         }
-        
+
         printf("End of C++ Socket Client Test\n");
-        
+
         return 0;
 }
