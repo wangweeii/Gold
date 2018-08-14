52140e94e28f6b2bc64784d85fdc39dad0c3e7d9
wangwei <wangweei@gmail.com>
2018-08-14 11:03:57 +0800

    Init

--- main.cc
+++ main.cc
@@ -8,28 +8,22 @@
 
 int main(int argc, char **argv)
 {
-        FILE *fp   = fopen("d:/EURUSD.csv", "r");
-        char line[60];
-        char delim = ',';
-        char *result;
-        fgets(line,60,fp);
-        printf("%s",line);
+        FILE       *fp     = fopen("d:/EURUSD.csv", "r");
+        char       line[60];
+        const char delim   = ',';
+        char       *result = nullptr;
 
         while (fgets(line, 60, fp))
         {
-                printf("%d %s", strlen(line), line);
-                line[strlen(line) - 1] = 0;
-                printf("%d %s\n", strlen(line), line);
+                printf("%s", line);
                 result = strtok(line, &delim);
-                printf("%s",result);
                 while (result)
                 {
-                        printf("%s\t", result);
+                        printf("%s ", result);
                         result = strtok(nullptr, &delim);
-                        // result=strtok(NULL,",");
                 }
-                printf("\n");
         }
+
         fclose(fp);
         printf("Shit");
         return 0;
