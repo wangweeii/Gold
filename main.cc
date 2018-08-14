dffe122d6909ea46a86ce11c9b09e5104ea542ae
wangwei <wangweei@gmail.com>
2018-08-14 17:11:03 +0800

    Init

--- main.cc
+++ main.cc
@@ -8,20 +8,21 @@
 
 int main(int argc, char **argv)
 {
-        FILE *fp = fopen("d:/tick/EURUSD.csv", "r");
-        char line[60];
-        char *result;
+        FILE        *fp = fopen("d:/tick/EURUSD.csv", "r");
+        char        line[60];
+        std::string symbol;
+        std::string time;
+        std::string Bid;
+        std::string Ask;
 
         while (fgets(line, 60, fp))
         {
-                line[strlen(line) - 1] = 0;
-                result = strtok(line, ",");
-                while (result)
-                {
-                        printf("%s ", result);
-                        result = strtok(nullptr, ",");
-                }
-                printf("\n");
+                // line[strlen(line) - 1] = 0;
+                symbol = strtok(line, ",");
+                time   = strtok(nullptr, ",");
+                Bid    = strtok(nullptr, ",");
+                Ask    = strtok(nullptr, ",");
+                printf("%s-%s: %g %g\n", symbol.c_str(), time.c_str(), atof(Bid.c_str()), atof(Ask.c_str()));
         }
 
         fclose(fp);
