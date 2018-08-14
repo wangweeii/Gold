79ea4064a7cd59fe4215bb99cfdbfece2d911552
wangwei <wangweei@gmail.com>
2018-08-14 10:51:52 +0800

    Init

--- main.cc
+++ main.cc
@@ -12,19 +12,25 @@ int main(int argc, char **argv)
         char line[60];
         char delim = ',';
         char *result;
+        fgets(line,60,fp);
+        printf("%s",line);
 
         while (fgets(line, 60, fp))
         {
-                // printf("%d %s", strlen(line), line);
+                printf("%d %s", strlen(line), line);
                 line[strlen(line) - 1] = 0;
-                // printf("%d %s\n", strlen(line), line);
+                printf("%d %s\n", strlen(line), line);
                 result = strtok(line, &delim);
+                printf("%s",result);
                 while (result)
                 {
                         printf("%s\t", result);
-                        result = strtok(NULL, &delim);
+                        result = strtok(nullptr, &delim);
+                        // result=strtok(NULL,",");
                 }
                 printf("\n");
         }
+        fclose(fp);
+        printf("Shit");
         return 0;
 }
