f51d820000505fb0000e77a57fd881c3e5d32346
wangwei <wangweei@gmail.com>
2018-08-26 01:12:42 +0800

    Moved backtest code

--- main.cc
+++ main.cc
@@ -2,13 +2,12 @@
 // Created by wangwei on 08/10.
 //
 
-#include <iostream>
-#include <dirent.h>
-#include <cstring>
-#include <string>
+// #include <iostream>
+// #include <dirent.h>
+// #include <cstring>
+// #include <string>
 #include "database.h"
-
-void back_test(MYSQL *db);
+#include "backtest.h"
 
 int main(int argc, char *argv[])
 {
@@ -33,27 +32,4 @@ int main(int argc, char *argv[])
         mysql_close(db);
         printf("Connect Closed.\n");
         return 0;
-}
-
-void back_test(MYSQL *db)
-{
-        std::string time;
-        double bid;
-        double ask;
-        double midpoint;
-        // long long max=315258147;
-        long long max = 10;
-        long long id = 0;
-
-        std::string sql = "select time, bid, ask from eurusd where id = ";
-        while(id++ < max)
-        {
-                sql.append(std::to_string(id));
-                sql += ';';
-                if (!query_line(db, sql.c_str()))
-                {
-                        break;
-                }
-                sql = "select id, time, bid, ask from eurusd where id = ";
-        }
 }
